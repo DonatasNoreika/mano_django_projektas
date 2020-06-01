e984a6ff33f84e9fe7c576b264d13c64de1dff46
Donatas Noreika <donoras@gmail.com>
2020-06-01 20:45:54 +0300

    10 pamoka - padarytas redaguojamas profilio puslapis

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -16,7 +16,7 @@ from django.contrib import messages
 
 # Importuojame FormMixin, kurį naudosime BookDetailView klasėje
 from django.views.generic.edit import FormMixin
-from .forms import BookReviewForm
+from .forms import BookReviewForm, UserUpdateForm, ProfilisUpdateForm
 
 # Create your views here.
 from django.http import HttpResponse
@@ -103,7 +103,23 @@ def register(request):
 
 @login_required
 def profilis(request):
-    return render(request, 'profilis.html')
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfilisUpdateForm(request.POST, request.FILES, instance=request.user.profilis)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f"Profilis atnaujintas")
+            return redirect('profilis')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfilisUpdateForm(instance=request.user.profilis)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+    }
+    return render(request, 'profilis.html', context)
 
 
 class BookListView(generic.ListView):
